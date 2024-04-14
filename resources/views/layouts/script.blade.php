e880d9222a7a16ba51e39ccdcd1d87b33aa7439d
ziaullhaq <ziaullhaq.arghandiwal75@gmail.com>
2024-04-14 12:33:34 +0430

    my first project

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,25 @@
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{ asset('assets/node_modules/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="{{ asset('assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="{{ asset('dist/js/perfect-scrollbar.jquery.min.js') }}"></script>
+    <!--Wave Effects -->
+    <script src="{{ asset('dist/js/waves.js') }}"></script>
+    <!--Menu sidebar -->
+    <script src="{{ asset('dist/js/sidebarmenu.js') }}"></script>
+    <!--Custom JavaScript -->
+    <script src="{{ asset('dist/js/custom.min.js') }}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!--morris JavaScript -->
+    <script src="{{ asset('assets/node_modules/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('assets/node_modules/morrisjs/morris.min.js') }}"></script>
+    <script src="{{ asset('assets/node_modules/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+    <!-- Popup message jquery -->
+    <script src="{{ asset('assets/node_modules/toast-master/js/jquery.toast.js') }}"></script>
+    <!-- Chart JS -->
+    <script src="{{ asset('dist/js/dashboard1.js') }}"></script>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+
+                <li> <a class=" waves-effect waves-dark" href="\" aria-expanded="false"><i class="icon-home"></i><span class="hide-menu">دشبورد</span></a>
+
+                </li>
+                <li> <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="icon-people"></i><span class="hide-menu">تنظیمات حساب کابری</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="app-calendar.html">سطح دسترسی</a></li>
+                        <li><a href="app-chat.html">سطوح دسترسی</a></li>
+                        <li><a href="app-ticket.html">کاربران</a></li>
+                        <li>
+                    </ul>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
